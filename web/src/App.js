1464119f898d884ab7208781befbe2fa39792818
Jhon Serna <jhonalfaber@gmail.com>
2021-10-15 12:01:43 -0500

    base

--- web/src/App.js
+++ web/src/App.js
@@ -20,12 +20,14 @@ import OwnerQuestionsPage from './pages/OwnerQuestionsPage'
 import { useAuthState } from "react-firebase-hooks/auth";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCTySyvuIDPg7RWF6ceuuwC2t3BEiAK38o",
-  authDomain: "question-app-demo.firebaseapp.com",
-  projectId: "question-app-demo",
-  storageBucket: "question-app-demo.appspot.com",
-  messagingSenderId: "1038673531562",
-  appId: "1:1038673531562:web:da90421f639a3115dcf6d3"
+  apiKey: "AIzaSyBSzStj3l9E54EjXk2OAVnC5Wme05X7njU",
+  authDomain: "questions-9fdd6.firebaseapp.com",
+  databaseURL: "https://questions-9fdd6-default-rtdb.firebaseio.com",
+  projectId: "questions-9fdd6",
+  storageBucket: "questions-9fdd6.appspot.com",
+  messagingSenderId: "633110626196",
+  appId: "1:633110626196:web:f9889fe72afa745e115981",
+  measurementId: "G-CL82NB276Y"
 });
 
 const auth = firebase.auth();
