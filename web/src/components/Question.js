e83a3aa6be1de56b8fdf64a7c21cc3fbfd334bab
Jhon Serna <jhonalfaber@gmail.com>
2021-10-20 13:08:01 -0500

    Modal implementado para eliminar pregunta

--- web/src/components/Question.js
+++ web/src/components/Question.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Modal } from 'reactstrap';
+import useModal from '../hooks/useModal';
 
-export const Question = ({ question, excerpt, onDelete }) => (
+export const Question = ({ question, excerpt, onDelete}) => {
+
+  const [isOpenModal, openModal, closeModal] = useModal();
+
+  return (
   <article className={excerpt ? 'question-excerpt' : 'question'}>
     <h2>{question.question}</h2>
     <p>{question.category}  - <small>{question.type}</small></p>
    
     {onDelete && (
-      <button className="button right" onClick={() => onDelete(question.id)}>DELETE</button>
+      <button className="button right" onClick={openModal}>DELETE</button>
     )}
     {excerpt && (
       <Link to={`/question/${question.id}`} className="button">
         View Question
       </Link>
     )}
+    <div>
+      <Modal
+      isOpen={isOpenModal}>
+        
+          <h3 className="pregunta text-center">Estas seguro de eliminar?</h3>
+          <div className="text-center">
+          <button className="btn_modal color1" onClick={() => onDelete(question.id)}>Si</button>
+          <button className="btn_modal color2" onClick={closeModal}>No</button>
+          </div>
+      </Modal>
+    </div>
   </article>
-)
+  )
+}
