3e0ee0ee82e3657b014ba5bf9c223dc7f8cde3db
Jhon Serna <jhonalfaber@gmail.com>
2021-10-22 11:48:58 -0500

    Se crea el test ListUseCaseTest

--- src/test/java/co/com/sofka/questions/usecases/ListUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/ListUseCaseTest.java
@@ -2,47 +2,50 @@ package co.com.sofka.questions.usecases;
 
 import co.com.sofka.questions.collections.Question;
 import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 
 import static org.mockito.Mockito.*;
 
+
+@SpringBootTest
 class ListUseCaseTest {
 
-     QuestionRepository repository;
+    @MockBean
+     QuestionRepository questionRepository;
+
+    @SpyBean
      ListUseCase listUseCase;
 
 
-    @BeforeEach
-    public void setup(){
-        MapperUtils mapperUtils = new MapperUtils();
-        repository = mock(QuestionRepository.class);
-        listUseCase = new ListUseCase(mapperUtils, repository);
-    }
+
 
     @Test
-     void getValidationTest(){
-        var question =  new Question();
-        question.setUserId("xxxx-xxxx");
-        question.setType("tech");
-        question.setCategory("software");
-        question.setQuestion("¿Que es java?");
-        when(repository.findAll()).thenReturn(Flux.just(question ));
+     void getAllTest(){
+        var question = new Question();
+        question.setId("01");
+        question.setUserId("u01");
+        question.setQuestion("test?");
+        question.setType("test");
+        question.setCategory("test");
 
-        StepVerifier.create(listUseCase.get())
-                .expectNextMatches(questionDTO -> {
-                    assert questionDTO.getUserId().equals("xxxx-xxxx");
-                    assert questionDTO.getCategory().equals("software");
-                    assert questionDTO.getQuestion().equals("¿Que es java?");
-                    assert questionDTO.getType().equals("tech");
-                    return true;
-                })
-                .verifyComplete();
+        Mockito.when(questionRepository.findAll()).thenReturn(Flux.just(question ));
 
-        verify(repository).findAll();
+        var resultQuestionDTO =  listUseCase.get().collectList().block();
+
+        Assertions.assertEquals(resultQuestionDTO.get(0).getId(), question.getId());
+        Assertions.assertEquals(resultQuestionDTO.get(0).getUserId(), question.getUserId());
+        Assertions.assertEquals(resultQuestionDTO.get(0).getQuestion(), question.getQuestion());
+        Assertions.assertEquals(resultQuestionDTO.get(0).getType(), question.getType());
+        Assertions.assertEquals(resultQuestionDTO.get(0).getCategory(), question.getCategory());
     }
 }
