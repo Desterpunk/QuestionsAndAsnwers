60c709b4441ccadb146033108d2df4985ca807e8
Jhon Serna <jhonalfaber@gmail.com>
2021-10-22 11:27:15 -0500

    Se crea el test FindUserByIdUseCaseTest

---
+++ src/test/java/co/com/sofka/questions/usecases/FindUserByIdUseCaseTest.java
@@ -0,0 +1,45 @@
+package co.com.sofka.questions.usecases;
+
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.collections.User;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.model.UserDTO;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import co.com.sofka.questions.reposioties.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class FindUserByIdUseCaseTest {
+
+    @MockBean
+    UserRepository userRepository;
+
+    @SpyBean
+    FindUserByIdUseCase findUserByIdUseCase;
+
+    @Test
+    public void findUserByIdUseCaseTest() {
+        var userDTO = new UserDTO("01","name","lastName?");
+        var user = new User();
+        user.setId("01");
+        user.setName("name");
+        user.setLastName("lastName");
+
+        Mockito.when(userRepository.findById(userDTO.getId())).thenReturn(Mono.just(user));
+
+        var resultUserDTO = findUserByIdUseCase.apply(user.getId()).block();
+
+        Assertions.assertEquals(resultUserDTO.getId(), user.getId());
+        Assertions.assertEquals(resultUserDTO.getName(), user.getName());
+        Assertions.assertEquals(resultUserDTO.getLastName(), user.getLastName());
+    }
+}
