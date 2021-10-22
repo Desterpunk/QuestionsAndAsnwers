2dc1657c5ad74035e1bcf494fa7ca922fff155f5
Jhon Serna <jhonalfaber@gmail.com>
2021-10-22 11:56:56 -0500

    Se crea el test OwnerAnswersListUseCaseTest

---
+++ src/test/java/co/com/sofka/questions/usecases/OwnerAnswersListUseCaseTest.java
@@ -0,0 +1,45 @@
+package co.com.sofka.questions.usecases;
+
+import co.com.sofka.questions.collections.Answer;
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.reposioties.AnswerRepository;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Flux;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class OwnerAnswersListUseCaseTest{
+
+    @MockBean
+    AnswerRepository answerRepository;
+
+    @SpyBean
+    OwnerAnswersListUseCase ownerAnswersListUseCase;
+
+    @Test
+    void ownerAnswersListTest(){
+        var answerDTO = new AnswerDTO("01","01","u01","test");
+        var answer = new Answer();
+        answer.setId("01");
+        answer.setQuestionId("01");
+        answer.setUserId("u01");
+        answer.setAnswer("test");
+
+        Mockito.when(answerRepository.findByUserId(answerDTO.getUserId())).thenReturn(Flux.just(answer));
+
+        var resultAnswerDTO =  ownerAnswersListUseCase.apply(answerDTO.getUserId()).collectList().block();
+
+        Assertions.assertEquals(resultAnswerDTO.get(0).getId(), answer.getId());
+        Assertions.assertEquals(resultAnswerDTO.get(0).getQuestionId(), answer.getQuestionId());
+        Assertions.assertEquals(resultAnswerDTO.get(0).getUserId(), answer.getUserId());
+        Assertions.assertEquals(resultAnswerDTO.get(0).getAnswer(), answer.getAnswer());
+    }
+}
